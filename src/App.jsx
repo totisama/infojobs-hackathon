f0e3a6ab482aa2db3bf0366f342cc06273980069
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-05-23 16:22:58 +0200

    Adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import './App.css'
+import ListOffers from './components/ListOffers'
+import { Logo } from './components/Logo'
 
 function App () {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return (
+    <>
+      <header className='flex flex-col items-center py-10'>
+        <Logo />
+        <h1 className='font-semibold tracking-wider text-2xl'>Road Map Generator</h1>
+      </header>
+      <main>
+        <ListOffers />
+      </main>
+    </>
+  )
 }
 
 export default App
