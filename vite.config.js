7d1b708a12ca84dc6cf99e5498f11c54cdbde6f3
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-05-23 16:22:20 +0200

    General project setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    proxy: {
+      '/api':
+      {
+        target: 'https://api.infojobs.net/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  plugins: [react()]
 })
