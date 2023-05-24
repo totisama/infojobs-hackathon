41663113a4891de87a8b6d3e63b8f827deb5039d
Rodrigo Samayoa <toti1998sama@gmail.com>
2023-05-24 20:31:35 +0200

    use new service function

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,5 +1,28 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { getOfferById } from '../services/getOfferById'
+
 const Offer = () => {
-  return <h1>Offer</h1>
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const [offer, setOffer] = useState({})
+
+  const getOffer = useCallback(async () => {
+    const oferta = await getOfferById(id)
+
+    if (oferta === '') {
+      navigate('/')
+
+      return
+    }
+    setOffer(oferta)
+  }, [id, navigate])
+
+  useEffect(() => {
+    getOffer()
+  }, [getOffer])
+
+  return <h1>Offer {offer?.title}</h1>
 }
 
 export default Offer
